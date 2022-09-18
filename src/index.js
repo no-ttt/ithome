1ff08ff0ef24d02757895c3808adbd079ed76061
no-ttt <dear890329@gmail.com>
2022-09-19 02:47:36 +0800

    init map

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import Main from './Main'
 import reportWebVitals from './reportWebVitals';
 
