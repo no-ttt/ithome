4ff3e5bd557a448d02501b66651cf66f2025586a
no-ttt <dear890329@gmail.com>
2022-09-20 03:29:12 +0800

    新增 Marker

--- src/Main.jsx
+++ src/Main.jsx
@@ -22,6 +22,30 @@ export default class Main extends Component {
       center: [lng, lat],
       zoom: zoom
     });
+
+    // // Create a new marker.
+    // const marker = new mapboxgl.Marker({
+    //   color: "pink",
+    //   rotation: 35
+    // })
+    //   .setLngLat([121, 23.5])
+    //   .addTo(map);
+
+    
+    // Create a DOM element for each marker.
+    const el = document.createElement('button');
+    const width = 40;
+    const height = 40;
+    el.className = 'marker';
+    el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`;
+    el.style.width = `${width}px`;
+    el.style.height = `${height}px`;
+    el.style.backgroundSize = '100%';
+
+    // Add markers to the map.
+    new mapboxgl.Marker(el)
+      .setLngLat([121, 23.5])
+      .addTo(map);
   }
 
   render() {
