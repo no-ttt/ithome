5bbe568623ba2eda9c4fa6452f04e997e0eda817
no-ttt <dear890329@gmail.com>
2022-09-20 23:41:30 +0800

    Control

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
-mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN'
+mapboxgl.accessToken = 'pk.eyJ1Ijoibm91c2VydXNlIiwiYSI6ImNsODR2dnJydTAxNXYzdnBzZWUwdWZkY3QifQ.TiXa2TtlGjSLY2gXNphj-w'
 
 export default class Main extends Component {
   constructor(props) {
@@ -16,36 +16,42 @@ export default class Main extends Component {
 
   componentDidMount() {
     const { lng, lat, zoom } = this.state;
-    const map = new mapboxgl.Map ({
+    // const map = new mapboxgl.Map ({
+    //   container: this.mapContainer.current,
+    //   style: 'mapbox://styles/mapbox/streets-v11',
+    //   center: [lng, lat],
+    //   zoom: zoom
+    // });
+
+    // AttributionControl
+    const map = new mapboxgl.Map({
       container: this.mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
-      zoom: zoom
-    });
+      zoom: zoom,
+      attributionControl: false, 
+    })
+      .addControl(new mapboxgl.AttributionControl({
+        customAttribution: 'Map design by me'
+      }));
 
-    // // Create a new marker.
-    // const marker = new mapboxgl.Marker({
-    //   color: "pink",
-    //   rotation: 35
-    // })
-    //   .setLngLat([121, 23.5])
-    //   .addTo(map);
 
-    
-    // Create a DOM element for each marker.
-    const el = document.createElement('button');
-    const width = 40;
-    const height = 40;
-    el.className = 'marker';
-    el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`;
-    el.style.width = `${width}px`;
-    el.style.height = `${height}px`;
-    el.style.backgroundSize = '100%';
+    // FullscreenControl
+    map.addControl(new mapboxgl.FullscreenControl());
 
-    // Add markers to the map.
-    new mapboxgl.Marker(el)
-      .setLngLat([121, 23.5])
-      .addTo(map);
+
+    //  GeolocateControl
+    map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserHeading: true,
+    }));
+
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
+
   }
 
   render() {
