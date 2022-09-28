afadaf5fbdb73385a3301905144952e0ed08d91a
no-ttt <dear890329@gmail.com>
2022-09-29 04:21:20 +0800

    move map

--- src/Main.jsx
+++ src/Main.jsx
@@ -7,72 +7,99 @@ export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lng: 120.3,
-      lat: 23.2,
-      zoom: 10,
+      data: [],
+      activeChapter: "",
     };
     this.mapContainer = React.createRef();
+    this.setActiveChapter = this.setActiveChapter.bind(this);
+    this.isElementOnScreen = this.isElementOnScreen.bind(this);
   }
 
   componentDidMount = async () =>  {
     // call api
-    const data = await fetch('https://tdx.transportdata.tw/api/basic/v2/Tourism/Restaurant/Tainan?%24format=JSON').then(response => response.json());
-    console.log(data)
-    const { lng, lat, zoom } = this.state;
+    const data = await fetch('https://tdx.transportdata.tw/api/basic/v2/Tourism/Restaurant/Taichung?%24top=20&%24format=JSON').then(response => response.json());
     
-
+    this.setState({
+      data: data,
+      activeChapter: data[0].RestaurantID
+    })
+    
     // init map
     const map = new mapboxgl.Map ({
       container: this.mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom,
-      attributionControl: false,
+      center: [data[0].Position.PositionLon, data[0].Position.PositionLat],
+      zoom: 15,
+      bearing: 27,
+      pitch: 45
     })
-      .addControl(new mapboxgl.AttributionControl({
-        customAttribution: 'My food map'
-      }));
 
-
-    // add marker
     data.map((d) => (
       new mapboxgl.Marker({
         scale: 0.8,
         color: "red"
       })
       .setLngLat([d.Position.PositionLon, d.Position.PositionLat])
-      .setPopup(new mapboxgl.Popup({  // add restaurant detail
-        maxWidth: "500px"
-      }).setHTML(`
-        <div class="res-detail-intro">
-          <img class="res-pic" src=${d.Picture.PictureUrl1 || "https://www.bomb01.com/upload/news/original/c95e0d21eda50ebc16d5f8ef568f60a7.png"} />
-          <div>
-            <div class="res-title-txt">${d.RestaurantName}</div>
-            <div class="res-detail-txt">營業時間：${d.OpenTime}</div>
-            <div class="res-detail-txt">地址：${d.Address}</div>
-            <div class="res-detail-txt">電話：${d.Phone}</div>
-        </div>
-        <div>
-      `))
       .addTo(map)
     ))
 
+    window.onscroll = () => {
+      for (let i = 0; i < data.length; i++) {
+        if (this.isElementOnScreen(data[i].RestaurantID)) {
+          this.setActiveChapter(data[i].RestaurantID, map, data[i].Position.PositionLon, data[i].Position.PositionLat);
+          break;
+        }
+      }
+    };
+  }
 
-    // add control
-    map.addControl(new mapboxgl.FullscreenControl());
-    map.addControl(new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true
-      },
-      trackUserLocation: true,
-      showUserHeading: true
-    }));
+  setActiveChapter = (chapterID, map, Lon, Lat) => {
+    const { activeChapter } = this.state
+
+    if (chapterID === activeChapter) return;
+
+    map.flyTo({
+      center: [Lon, Lat],
+      zoom: 18
+    })
+
+    document.getElementById(chapterID).classList.add('active');
+    document.getElementById(activeChapter).classList.remove('active');
+
+    this.setState({
+      activeChapter: chapterID
+    })
+  }
+
+  isElementOnScreen = (id) => {
+    const element = document.getElementById(id);
+    const bounds = element.getBoundingClientRect();
+
+    return bounds.top < window.innerHeight && bounds.bottom > 0;
   }
 
   render() {
+    const { data } = this.state;
     return (
       <div>
         <div ref={this.mapContainer} className="map-container" />
+        <div id="features">
+          {
+            data.map((d, i) => (
+              <section key={i} id={d.RestaurantID} className={ i === 0 ? "active": "" }>
+                <h3>{d.RestaurantName}</h3>
+                <div className="res-detail-intro">
+                  <div>
+                    <div className="res-detail-txt">營業時間：{d.OpenTime}</div>
+                    <div className="res-detail-txt">地址：{d.Address}</div>
+                    <div className="res-detail-txt">電話：{d.Phone}</div>
+                  </div>
+                </div>
+              </section>
+            ))
+          }
+          <section style={{ paddingBottom: "500px"}} />
+        </div>
       </div>
     );
   }
