86124713073b3b757a6c48df5507b2c1cc6b8107
no-ttt <dear890329@gmail.com>
2022-10-03 21:52:37 +0800

    cluster layer

--- src/Main.jsx
+++ src/Main.jsx
@@ -10,7 +10,7 @@ export default class Main extends Component {
     this.state = {
       lng: 120.2,
       lat: 23.2,
-      zoom: 9,
+      zoom: 8,
     };
     this.mapContainer = React.createRef();
   }
@@ -29,19 +29,80 @@ export default class Main extends Component {
     map.on('load', () => {
       map.addSource('restaurant', {
         type: 'geojson',
-        data: output
+        data: output,
+        cluster: true,
+        clusterMaxZoom: 14,
+        clusterRadius: 80
+      });
+       
+      map.addLayer({
+        id: 'clusters',
+        type: 'circle',
+        source: 'restaurant',
+        filter: ['has', 'point_count'],
+        paint: {
+          'circle-color': [
+            'step',
+            ['get', 'point_count'],
+            '#51bbd6',
+            100,
+            '#f1f075',
+            250,
+            '#f28cb1'
+          ],
+          'circle-radius': [
+            'step',
+            ['get', 'point_count'],
+            20,
+            100,
+            30,
+            750,
+            40
+          ]
+        }
+      });
+       
+      map.addLayer({
+        id: 'cluster-count',
+        type: 'symbol',
+        source: 'restaurant',
+        filter: ['has', 'point_count'],
+        layout: {
+          'text-field': '{point_count_abbreviated}',
+          'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+          'text-size': 12
+        }
       });
        
       map.addLayer({
         id: 'unclustered-point',
         type: 'circle',
         source: 'restaurant',
+        filter: ['!', ['has', 'point_count']],
         paint: {
           'circle-color': 'red',
           'circle-radius': 4,
           'circle-stroke-width': 1,
           'circle-stroke-color': '#fff'
         }
+      });
+       
+      map.on('click', 'clusters', (e) => {
+        const features = map.queryRenderedFeatures(e.point, {
+          layers: ['clusters']
+        });
+        const clusterId = features[0].properties.cluster_id;
+        map.getSource('restaurant').getClusterExpansionZoom(
+          clusterId,
+          (err, zoom) => {
+            if (err) return;
+        
+            map.easeTo({
+              center: features[0].geometry.coordinates,
+              zoom: zoom
+            });
+         }
+        );
       });
     });
   }
