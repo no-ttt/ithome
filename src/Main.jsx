fe51bdcac8f6442a53389731e40caca6368ca8cb
no-ttt <dear890329@gmail.com>
2022-10-03 21:35:10 +0800

    circle layer

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,72 +1,49 @@
 import React, { Component } from "react"
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import output from './output.json'
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoibm91c2VydXNlIiwiYSI6ImNsODR2dnJydTAxNXYzdnBzZWUwdWZkY3QifQ.TiXa2TtlGjSLY2gXNphj-w'
+mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN'
 
 export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lng: 120.3,
+      lng: 120.2,
       lat: 23.2,
-      zoom: 10,
+      zoom: 9,
     };
     this.mapContainer = React.createRef();
   }
 
   componentDidMount = async () =>  {
-    // call api
-    const data = await fetch('https://tdx.transportdata.tw/api/basic/v2/Tourism/Restaurant/Tainan?%24format=JSON').then(response => response.json());
-    console.log(data)
     const { lng, lat, zoom } = this.state;
     
-
     // init map
     const map = new mapboxgl.Map ({
       container: this.mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
       zoom: zoom,
-      attributionControl: false,
     })
-      .addControl(new mapboxgl.AttributionControl({
-        customAttribution: 'My food map'
-      }));
 
-
-    // add marker
-    data.map((d) => (
-      new mapboxgl.Marker({
-        scale: 0.8,
-        color: "red"
-      })
-      .setLngLat([d.Position.PositionLon, d.Position.PositionLat])
-      .setPopup(new mapboxgl.Popup({  // add restaurant detail
-        maxWidth: "500px"
-      }).setHTML(`
-        <div class="res-detail-intro">
-          <img class="res-pic" src=${d.Picture.PictureUrl1 || "https://www.bomb01.com/upload/news/original/c95e0d21eda50ebc16d5f8ef568f60a7.png"} />
-          <div>
-            <div class="res-title-txt">${d.RestaurantName}</div>
-            <div class="res-detail-txt">營業時間：${d.OpenTime}</div>
-            <div class="res-detail-txt">地址：${d.Address}</div>
-            <div class="res-detail-txt">電話：${d.Phone}</div>
-        </div>
-        <div>
-      `))
-      .addTo(map)
-    ))
-
-
-    // add control
-    map.addControl(new mapboxgl.FullscreenControl());
-    map.addControl(new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true
-      },
-      trackUserLocation: true,
-      showUserHeading: true
-    }));
+    map.on('load', () => {
+      map.addSource('restaurant', {
+        type: 'geojson',
+        data: output
+      });
+       
+      map.addLayer({
+        id: 'unclustered-point',
+        type: 'circle',
+        source: 'restaurant',
+        paint: {
+          'circle-color': 'red',
+          'circle-radius': 4,
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#fff'
+        }
+      });
+    });
   }
 
   render() {
