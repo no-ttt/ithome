e04f231a2a14ac5f0521a69ab1f7d6cdfebd1b92
no-ttt <dear890329@gmail.com>
2022-10-08 22:45:36 +0800

    application

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,161 +1,189 @@
 import React, { Component } from "react"
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import CardMedia from '@mui/material/CardMedia'
+import DialogTitle from '@mui/material/DialogTitle'
+import Dialog from '@mui/material/Dialog'
+import TextField from '@mui/material/TextField'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import AddIcon from '@mui/icons-material/Add'
 
 mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN'
-let markers = []
 
 export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lng: -122.662323,
-      lat: 45.523751,
-      zoom: 12,
-			map: null,
-			start: [-122.662323, 45.523751],
-			end: [-122.632282, 45.553088],
-			traffic: "driving",
-			costTime: 0,
+      open: false,
+      data: [],
+      map: null,
+      name: "",
+      des: "",
+      lat: "",
+      lng: "",
+      img: "",
+      waypoint: "",
+      traffic: "driving"
     };
     this.mapContainer = React.createRef();
   }
 
   componentDidMount() {
-    const { lng, lat, zoom } = this.state;
     const map = new mapboxgl.Map ({
       container: this.mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
+      center: [120.5, 23.5],
+      zoom: 7
     });
 
-		this.setState({ map })
+    this.setState({ map: map })
   }
 
-	componentDidUpdate(prevProps, prevState) {
-		const { map, start, end, traffic } = this.state;
-		console.log(start, end, traffic)
+  async getRoute(waypoint, traffic) {
+    const { map } = this.state
+    const query = await fetch(
+      `https://api.mapbox.com/directions/v5/mapbox/${traffic}/${waypoint}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+      { method: 'GET' }
+    );
+    const json = await query.json();
+    const data = json.routes[0];
+    const route = data.geometry.coordinates;
+    const geojson = {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'LineString',
+        coordinates: route
+      }
+    };
 
-		markers.forEach((marker) => marker.remove())
+    if (map.getSource('route')) {
+      map.getSource('route').setData(geojson);
+    }
+    else {
+      map.addLayer({
+        id: 'route',
+        type: 'line',
+        source: {
+          type: 'geojson',
+          data: geojson
+        },
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#3887be',
+          'line-width': 5,
+          'line-opacity': 0.75
+        }
+      });
+    }
+  }
 
-		// Create a DOM element for each marker.
-		const el = document.createElement('button');
-		// el.innerHTML = "start";
-		const width = 20;
-		const height = 20;
-		el.className = 'marker';
-		el.style.width = `${width}px`;
-		el.style.height = `${height}px`;
-		el.style.backgroundSize = '100%';
-	
-		// Add markers to the map.
-		const marker = new mapboxgl.Marker(el)
-			.setLngLat(start)
-			.addTo(map);
+  submit = () => {
+    const { map, data, name, des, lat, lng, img, waypoint, traffic } = this.state
+    const newData = {
+      name: name,
+      des: des,
+      lat: lat,
+      lng: lng,
+      img: img
+    }
 
-		const el2 = document.createElement('button');
-		// el.innerHTML = "start";
-		el2.className = 'marker2';
-		el2.style.width = `${width}px`;
-		el2.style.height = `${height}px`;
-		el2.style.backgroundSize = '100%';
-
-		// // Add markers to the map.
-		const marker2 = new mapboxgl.Marker(el2)
-			.setLngLat(end)
-			.addTo(map);
-		
-		this.getRoute(map, start, end, traffic)
-		markers.push(marker)
-		markers.push(marker2)
-	}
-
-
-	async getRoute(map, start, end, traffic) {
-		const query = await fetch(
-			`https://api.mapbox.com/directions/v5/mapbox/${traffic}/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
-			{ method: 'GET' }
-		)
-		
-		const json = await query.json()
-		const data = json.routes[0]
-		const route = data.geometry.coordinates
-		const geojson = {
-			type: 'Feature',
-			properties: {},
-			geometry: {
-				type: 'LineString',
-				coordinates: route
-			}
-		}
-
-		let index = 0
-
-		if (index === 0) {
-			this.setState({
-				costTime: Math.floor(data.legs[0].duration / 60)
-			})
-			index += 1
-		}
-
-
-		// console.log(Math.floor(data.legs[0].duration / 60))
-
-		if (map.getSource('route')) {
-			map.getSource('route').setData(geojson);
-		}
-		else {
-			map.addLayer({
-				id: 'route',
-				type: 'line',
-				source: {
-					type: 'geojson',
-					data: geojson
-				},
-				layout: {
-					'line-join': 'round',
-					'line-cap': 'round'
-				},
-				paint: {
-					'line-color': '#3887be',
-					'line-width': 5,
-					'line-opacity': 0.75
-				}
-			});
-		}
-	}
+    let newWaypoint = ""
+    if (waypoint !== "") {
+      newWaypoint = waypoint + ';' + lng.toString() + ',' + lat.toString()
+      this.getRoute(newWaypoint, traffic);
+      new mapboxgl.Marker({
+        color: "red"
+      })
+        .setLngLat([lng, lat])
+        .setPopup(new mapboxgl.Popup().setHTML(`<div>${name}</div>`))
+        .addTo(map)
+    }
+    else {
+      newWaypoint = lng.toString() + ',' + lat.toString()
+      new mapboxgl.Marker({
+        color: "red"
+      })
+      .setLngLat([lng, lat])
+      .setPopup(new mapboxgl.Popup().setHTML(`<div>${name}</div>`))
+      .addTo(map)
+    }
+    
+    this.setState({
+      data: [...data, newData],
+      open: false, 
+      name: "",
+      des: "",
+      lat: "",
+      lng: "",
+      img: "",
+      waypoint: newWaypoint
+    })
+  }
 
   render() {
-		const { start, end, costTime } = this.state
+    const { open, data, traffic, waypoint } = this.state
     return (
       <div>
         <div ref={this.mapContainer} className="map-container" />
-				<div id="instructions">
-					<div style={{ marginBottom: "10px" }}>
-						<label>Start Position : </label>
-						<input type="text" id="start" name="start" value={start} style={{ width: "150px" }}
-							onChange={e => { this.setState({ start: [e.target.value.split(",")[0], e.target.value.split(",")[1]] })}} 
-							/>
-					</div>
-					<div style={{ marginBottom: "10px" }}>
-						<label>End Position : </label>
-						<input type="text" id="end" name="end" value={end} style={{ width: "150px" }}
-							onChange={e => { this.setState({ end: [e.target.value.split(",")[0], e.target.value.split(",")[1]] })}} 
-						/>
-					</div>
-					<div style={{ marginBottom: "10px" }}>
-						<label>Traffic : </label>
-						<select defaultValue="driving" onChange={e => this.setState({ traffic: e.target.value })}>
-							<option value="driving">driving</option>
-							<option value="walking">walking</option>
-							<option value="cycling">cycling</option>
-						</select>
-					</div>
-					<div>
-						<span>Spend Time : </span>
-						<span>{costTime} mins</span>
-					</div>
-				</div>
+        <div id="content">
+          <Card sx={{ display: 'flex', justifyContent: 'space-between', alignItems: "center", width: 280, height: 30, padding: "10px", marginBottom: "15px" }}>
+            <div>交通工具</div>
+            <Select value={traffic} size="small" onChange={(e) => { this.setState({ traffic: e.target.value }); this.getRoute(waypoint, e.target.value) }}>
+              <MenuItem value="driving-traffic">driving-traffic</MenuItem>
+              <MenuItem value="driving">driving</MenuItem>
+              <MenuItem value="walking">walking</MenuItem>
+              <MenuItem value="cycling">cycling</MenuItem>
+            </Select>
+          </Card>
+          {
+            data.map((d, i) => (
+              <Card key={i} sx={{ display: 'flex', justifyContent: 'space-between', width: 300, height: 100, marginBottom: "15px" }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <CardContent>
+                    <Typography component="div" variant="h6">{d.name}</Typography>
+                    <Typography variant="subtitle" color="text.secondary" component="p">{d.des}</Typography>
+                  </CardContent>
+                </Box>
+                <CardMedia component="img" sx={{ width: 100, height: 100 }} image={d.img} />
+              </Card>
+            ))
+          }
+          <button className="icon-btn" onClick={() => this.setState({ open: true })}><AddIcon /></button>
+          <Dialog onClose={() => this.setState({ open: false })} open={open}>
+            <DialogTitle>輸入你要加入的景點資訊</DialogTitle>
+            <div style={{ padding: "20px" }}>
+              <div className="input-info">
+                <span>景點名稱</span>
+                <TextField id="name" label="name" variant="outlined" size="small" onChange={(e) => this.setState({ name: e.target.value })} />
+              </div>
+              <div className="input-info">
+                <span>備註</span>
+                <TextField id="des" label="des" variant="outlined" size="small" onChange={(e) => this.setState({ des: e.target.value })} />
+              </div>
+              <div className="input-info">
+                <span>經度</span>
+                <TextField id="longitude" label="longitude" variant="outlined" size="small" onChange={(e) => this.setState({ lng: e.target.value })} />
+              </div>
+              <div className="input-info">
+                <span>緯度</span>
+                <TextField id="latitude" label="latitude" variant="outlined" size="small" onChange={(e) => this.setState({ lat: e.target.value })} />
+              </div>
+              <div className="input-info">
+                <span>景點照片</span>
+                <TextField id="img" label="img" variant="outlined" size="small" onChange={(e) => this.setState({ img: e.target.value })} />
+              </div>
+              <button className="add-btn" onClick={this.submit}>Add</button>
+            </div>
+          </Dialog>
+        </div>
       </div>
     );
   }
